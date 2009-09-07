5094a17f81ad90c25f432c18b2d1e96b42a92dba
Mike Redd <mikered@hotmail.com>
2009-09-07 08:58:26 -0700

    modified:   scripts/site-grab.sh

--- scripts/site-grab.sh
+++ scripts/site-grab.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # Site Grab
+VER="0.1"
 # http://script.m-redd.com
 # Smaller than Life Projects
 # By: MreDD
 # projects - at - 0tue0 (dot) com
 #################################
+# Version
+echo ""
+echo "Site Grab V. $VER"
+echo ""
+echo ""
 # Check WGet
 if [[ -z $( type -p wget ) ]]; then 
       echo -e "wget -- NOT INSTALLED !";exit
 fi
 WHEREISWGET=$(whereis wget | awk '{print $2}')
 WGET="$WHEREISWGET"
+# Check for Site Grab's Working Directory
 LSGETDIR=$(ls $HOME/dev/wget | grep "sites")
 if [ "$LSGETDIR" != "sites" ];then
      mkdir -p "$HOME/dev/wget/sites"
